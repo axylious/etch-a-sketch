e8ec550289c0fafe1927ea098bb57f9eb2686184
Axel Olivas <axelbrolivas@gmail.com>
2022-02-16 12:45:42 -0800

    finished rgb etch-a-sketch

--- board.js
+++ board.js
@@ -37,7 +37,11 @@ function removeBoard() {
 }
 
 function fillBlock(e) {
-    this.style['background-color'] = 'black'
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+
+    this.style['background-color'] = `rgb(${r},${g},${b})`
 }
 
 function clearForm() {
