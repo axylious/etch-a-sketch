7ac42cdbea272d2262e2ff9a85a711c4d781bb96
Axel Olivas <axelbrolivas@gmail.com>
2022-02-16 12:40:06 -0800

    finished etch-a-sketch

---
+++ board.js
@@ -0,0 +1,51 @@
+const board = document.createElement('div')
+board.className = 'board'
+
+function createBoard(num) {
+    const boardSize = num || 16
+    
+    const container = document.getElementById('container')
+    const docFrag = document.createDocumentFragment()
+    
+    for(i=0; i<boardSize; i++){
+        for(j=0; j<boardSize; j++){
+            let block = document.createElement('div')
+            block.className ='block'
+            docFrag.appendChild(block)
+        }
+    }
+    board.setAttribute('style',
+        `grid-template-columns: repeat(${boardSize}, 1fr);
+        grid-template-rows: repeat(${boardSize}, 1fr);`
+    )
+    
+    board.appendChild(docFrag)
+    
+    container.appendChild(board)
+
+    const blocks = document.getElementsByClassName('block')
+    Object.values(blocks).forEach(block => {
+        block.addEventListener('mouseover', fillBlock)
+    })
+}
+createBoard()
+
+function removeBoard() {
+    while(board.firstChild) {
+        board.removeChild(board.firstChild)
+    }
+}
+
+function fillBlock(e) {
+    this.style['background-color'] = 'black'
+}
+
+function clearForm() {
+    let num = 0
+    do {
+        num = Number(window.prompt('Please enter a number:', '[1-100]'))
+    } while(num<1 || num>100)
+
+    removeBoard()
+    createBoard(num)
+}
